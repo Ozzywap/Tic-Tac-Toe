739cc4c108e1708f72453855dd36113b55c74e71
Osman Haji <osman6191@outlook.com>
2023-06-02 23:57:39 +1000

    added initial gameBoard tests
    Added tests for the different win conditions as well as ties and games with no wins.

--- app/src/test/java/com/example/tictactoe/GameBoardTest.java
+++ app/src/test/java/com/example/tictactoe/GameBoardTest.java
@@ -4,6 +4,7 @@ import android.os.Build;
 
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -13,28 +14,94 @@ import org.robolectric.annotation.Config;
 
 
 public class GameBoardTest {
+    private MainActivity mainActivity = new MainActivity();
+    public void resetGameBoard(){
+        for (int i = 0; i < mainActivity.gameBoard.length; i++) {
+            for (int j = 0; j < mainActivity.gameBoard.length; j++) {
+                mainActivity.gameBoard[i][j] = 0;
+            }
+        }
+    }
 
     @Test
     public void testEmptyGameBoard(){
-        MainActivity mainActivity = new MainActivity();
-//        for(int i = 0; i <mainActivity.gameBoard.length; i++){
-//            for(int j = 0; j < mainActivity.gameBoard.length; j++){
-//                mainActivity.gameBoard[i][j] = MainActivity.EMPTY;
-//            }
-//        }
-//        mainActivity.gameBoard[0][0] = 1;
-
-        mainActivity.gameBoard[0][0] = 1;
-        mainActivity.gameBoard[0][1] = 2;
-        mainActivity.gameBoard[0][2] = 1;
-        mainActivity.gameBoard[1][0] = 2;
-        mainActivity.gameBoard[1][1] = 1;
-        mainActivity.gameBoard[1][2] = 2;
-        mainActivity.gameBoard[2][0] = 2;
-        mainActivity.gameBoard[2][1] = 1;
-        mainActivity.gameBoard[2][2] = 2;
-
-
+        // all cells are by default 0
         assertFalse(mainActivity.checkWin());
+    }
+
+    @Test
+    public void testTie(){
+        mainActivity.gameBoard[0][0] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[0][1] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[0][2] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[1][0] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[1][1] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[1][2] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[2][0] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[2][1] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[2][2] = MainActivity.PLAYER_O;
+        assertFalse(mainActivity.checkWin());
+    }
+
+    @Test
+    public void testRowWins(){
+        // test row 1 win
+        mainActivity.gameBoard[0][0] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[0][1] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[0][2] = MainActivity.PLAYER_X;
+        assertTrue(mainActivity.checkWin());
+
+        // test row 2 win
+        resetGameBoard();
+        mainActivity.gameBoard[1][0] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[1][1] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[1][2] = MainActivity.PLAYER_O;
+        assertTrue(mainActivity.checkWin());
+
+        // test row 3 win
+        resetGameBoard();
+        mainActivity.gameBoard[2][0] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[2][1] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[2][2] = MainActivity.PLAYER_X;
+        assertTrue(mainActivity.checkWin());
+    }
+
+    @Test
+    public void testColumnWins(){
+        // test column 1 win
+        mainActivity.gameBoard[0][0] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[1][0] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[2][0] = MainActivity.PLAYER_X;
+        assertTrue(mainActivity.checkWin());
+
+        // test column 2 win
+        resetGameBoard();
+        mainActivity.gameBoard[0][1] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[1][1] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[2][1] = MainActivity.PLAYER_O;
+        assertTrue(mainActivity.checkWin());
+
+        // test column 3 win
+        resetGameBoard();
+        mainActivity.gameBoard[0][2] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[1][2] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[2][2] = MainActivity.PLAYER_X;
+        assertTrue(mainActivity.checkWin());
+    }
+
+    @Test
+    public void testDiagonalWins(){
+        // test column 1 win
+        mainActivity.gameBoard[0][0] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[1][1] = MainActivity.PLAYER_X;
+        mainActivity.gameBoard[2][2] = MainActivity.PLAYER_X;
+        assertTrue(mainActivity.checkWin());
+
+        // test column 2 win
+        resetGameBoard();
+        mainActivity.gameBoard[0][2] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[1][1] = MainActivity.PLAYER_O;
+        mainActivity.gameBoard[2][0] = MainActivity.PLAYER_O;
+        assertTrue(mainActivity.checkWin());
     }
 }
